898908fadaf0f9c3dfdeabbdc936673d807c2ed8
Ruslan Gizatulin <gizatulinrr@gmail.com>
2021-07-17 21:15:38 +0500

    add monitor docker metrics

--- ec2.tf
+++ ec2.tf
@@ -97,6 +97,12 @@ resource "aws_security_group" "sg_22" {
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
+   ingress {
+      from_port   = 9323
+      to_port     = 9323
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
  egress {
     from_port   = 0
     to_port     = 0
