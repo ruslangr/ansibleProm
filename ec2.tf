16bbab065257fcf20abd685b85e14ee519178d57
Ruslan Gizatulin <gizatulinrr@gmail.com>
2021-07-17 12:10:20 +0500

    add terraform config

---
+++ ec2.tf
@@ -0,0 +1,103 @@
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default = "10.1.0.0/24"
+}
+variable "availability_zone" {
+  description = "availability zone to create subnet"
+  default = "us-west-2a"
+}
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default = "ami-0dc8f589abe99f538"
+}
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default = "t2.medium"
+}
+variable "environment_tag" {
+  description = "Environment tag"
+  default = "Production"
+}
+
+provider "aws" {
+  profile	= "default"
+  region	= "us-west-2"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.cidr_vpc}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "${var.cidr_subnet}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${var.availability_zone}"
+  tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+
+resource "aws_route_table" "rtb_public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = "${aws_subnet.subnet_public.id}"
+  route_table_id = "${aws_route_table.rtb_public.id}"
+}
+
+resource "aws_security_group" "sg_22" {
+  name = "sg_22"
+  vpc_id = "${aws_vpc.vpc.id}"
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+
+resource "aws_instance" "testInstance" {
+  ami           = "${var.instance_ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "${aws_subnet.subnet_public.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
+  key_name = "adm2_oregon"
+ tags =  {
+  Environment = "${var.environment_tag}"
+ }
+}
+
